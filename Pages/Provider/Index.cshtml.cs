5794a0046ae19faa1e6cc0a651b5d4f4dfd5162f
Oenh824029 <oskarnaranjo_123@outlook.com>
2024-11-09 00:42:28 -0500

    create provider

---
+++ Pages/Provider/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Provider
+{
+    //[Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Providers> Provider { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            // condición para las categorias
+            if (_context.Providers != null)
+            {
+                Provider = await _context.Providers.ToListAsync();
+            }
+        }
+    }
+}
